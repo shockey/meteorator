3c5f838e1d439f1cbc8c883aedd374dbd760abdf
Kyle Shockey <kyleshockey1@gmail.com>
2015-04-27 20:08:17 -0700

    improvements to forum view

--- client/templateHelpers.js
+++ client/templateHelpers.js
@@ -1,7 +1,15 @@
 //////// HELPERS
 
 Template.forum.helpers({
-  
+  questions: function(){
+    return Questions.find({forum: this._id});
+  },
+  friendlyUsername: function() {
+    return Meteor.users.findOne({_id: this.asker}).profile.name;
+  },
+  isAsker: function(){
+    return !!(Meteor.userId() === this.asker)
+  }
 })
 
 Template.nav.helpers({
@@ -35,7 +43,24 @@ Template.forums.events({
     console.log(Forums);
     Forums.remove({_id: this._id});
   }
-})
+});
+
+Template.forum.events({
+  'click .submit': function(){
+    var content = document.getElementById('newQuestion');
+
+    Questions.insert({
+      content: content.value,
+      asker: Meteor.userId(),
+      forum: this._id
+    }, function(){
+      content.value = "";
+    })
+  },
+  'click .deleteQ': function(){
+    Questions.remove({_id: this._id});
+  }
+});
 
 Template.createNew.events({
   'click .submit': function(){
