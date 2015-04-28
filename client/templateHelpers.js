9b704122003fadda69035a9f3a4ba09690433dfa
Kyle Shockey <kyleshockey1@gmail.com>
2015-04-27 17:50:06 -0700

    extend functionality of forums template

--- client/templateHelpers.js
+++ client/templateHelpers.js
@@ -16,5 +16,20 @@ Template.nav.helpers({
 Template.forums.helpers({
   forums: function(){
     return Forums.find();
+  },
+  friendlyUsername: function() {
+    return Meteor.users.findOne({_id: this.owner}).profile.name;
+  }
+});
+
+Template.createNew.events({
+  'click .submit': function(){
+    var title = document.getElementById('newForumTitle').value;
+    var desc = document.getElementById('newForumDescription').value;
+    Forums.insert({
+      title: title,
+      description: desc,
+      owner: Meteor.userId()
+    });
   }
 });
