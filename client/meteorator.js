085c986394cbcff5499d9f886566407e6c28730a
Kyle Shockey <kyleshockey1@gmail.com>
2015-05-19 09:21:30 -0700

    improve credits

--- client/meteorator.js
+++ client/meteorator.js
@@ -1,18 +1,11 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-  /*
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  $( document ).ready(function() {
+    $('body').append(
+      '<a href="http://hackreactor.com"> \
+      <img style="position: fixed; top: 0; right: 0; border: 0;" \
+      src="http://i.imgur.com/x86kKmF.png" \
+      alt="Built at Hack Reactor"> \
+      </a>'
+    );
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-  */
 }
